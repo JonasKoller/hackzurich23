2f453e6e685714d0a896acde4cef7fd664c39be2
davidgataric <daavidgtr@outlook.com>
2023-09-16 20:51:06 +0200

    sort by buying power added

--- frontend/src/app/features/shop/shop/shop.component.ts
+++ frontend/src/app/features/shop/shop/shop.component.ts
@@ -8,4 +8,22 @@ import {Component, Input} from '@angular/core';
 export class ShopComponent {
   @Input() points: number;
   @Input() shopItems: any;
+
+  sortByBuyingPower() {
+    if (!this.shopItems) {
+      return [];
+    }
+    // @ts-ignore
+    return this.shopItems.sort((a, b) => {
+      if ((a.price <= this.points && b.price <= this.points) || (a.price > this.points && b.price > this.points)) {
+        return a.price - b.price;
+      }
+
+      if (a.price <= this.points) return -1;
+
+      if (b.price <= this.points) return 1;
+
+      return 0;
+    });
+  }
 }
