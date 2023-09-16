e74be3dc951ece98e9f029d677f1c8c32143c0cb
JonasKoller <jonas.koller@gmx.ch>
2023-09-17 00:59:26 +0200

    optimize new feature

--- frontend/src/app/features/follow-me/follow-me/follow-me.component.ts
+++ frontend/src/app/features/follow-me/follow-me/follow-me.component.ts
@@ -10,6 +10,8 @@ export class FollowMeComponent {
 
   sliderValue = 0;
   coinsEarned = 0;
+  currentProgress = 0;
+
 
   async change($event: Event) {
     this.sliderValue = ($event.target as any).value;
@@ -17,15 +19,33 @@ export class FollowMeComponent {
     if (this.sliderValue <= 250) {
       for (let i = this.sliderValue; i > 0; i--) {
         ($event.target as any).value = i;
-        await delay(100);
+        await this.delay(1);
       }
       this.sliderValue = 0;
     } else {
       for (let i = this.sliderValue; i < 500; i++) {
         ($event.target as any).value = i;
-        await delay(100);
+        await this.delay(1);
       }
       this.sliderValue = 500;
+      setTimeout(() => this.earnPoints(), 0);
     }
+    this.currentProgress = 0;
+    this.coinsEarned = 0;
+  }
+
+  async earnPoints() {
+    while (this.sliderValue == 500) {
+      await this.delay(1000);
+      this.currentProgress++;
+      if (this.currentProgress == 100) {
+        this.coinsEarned++;
+        this.currentProgress = 0;
+      }
+    }
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 }
