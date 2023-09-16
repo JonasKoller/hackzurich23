19feae25e53c79d6b38fac383b3a0ba0899e8610
JonasKoller <jonas.koller@gmx.ch>
2023-09-16 05:14:53 +0200

    added autocompletion to from and to

--- frontend/src/app/features/route-planner/route-planner/route-planner.component.ts
+++ frontend/src/app/features/route-planner/route-planner/route-planner.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import AutocompleteOptions = google.maps.places.AutocompleteOptions;
 
 @Component({
   selector: 'app-route-planner',
   templateUrl: './route-planner.component.html',
   styleUrls: ['./route-planner.component.scss']
 })
-export class RoutePlannerComponent {
+export class RoutePlannerComponent implements AfterViewInit {
+
+  @ViewChild('fromInput', { static: false }) fromInputHtmlElement: ElementRef;
+  @ViewChild('toInput', { static: false }) toInputHtmlElement: ElementRef;
+
+  @Output() fromChanged = new EventEmitter<string>();
+  @Output() toChanged = new EventEmitter<string>();
+
+  ngAfterViewInit(): void {
+    const options: AutocompleteOptions = {
+      componentRestrictions: { country: 'ch' },
+      fields: ['address_components', 'geometry', 'name'],
+      strictBounds: false
+    };
+
+    const autocompleteFrom = new google.maps.places.Autocomplete(this.fromInputHtmlElement.nativeElement, options);
+    autocompleteFrom.addListener('place_changed', () => this.fromChanged.emit(this.fromInputHtmlElement.nativeElement.value));
+    const autocompleteTo = new google.maps.places.Autocomplete(this.toInputHtmlElement.nativeElement, options);
+    autocompleteTo.addListener('place_changed', () => this.toChanged.emit(this.toInputHtmlElement.nativeElement.value));
+  }
 
 }
